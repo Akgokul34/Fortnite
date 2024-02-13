5c02959ad16af4d07dce206e65d23828373d4e0d
gokul200204 <126974926+gokul200204@users.noreply.github.com>
2024-02-13 18:40:21 +0530

    Fortnite

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="background">
+        <div>
+        <h1 className='heading'>Epic Games:An American video Games And Software Developer And Publisher Based in Cary,North Carolina.</h1>
+        </div>
+      <div>
+        <img src="https://s3-alpha-sig.figma.com/img/e623/6bd7/1dd8e61fcac2c9c7af98f1fbbf93d497?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jqym85cLEJ7T5~k~Z4g5Z1kgIGpd-Rmlvsv-uYLaTo2JKGvz-x24XGLHs8wIngtbcsbIbTCj6p0zInpkQMIWffRpc0nl~lpYcB45l2eeGo8QdowA2pVRWPkZ0hAi2MSOlGz-TQBBtqbUZosNvjrfJbg-aQfXG3eNDdvUHLLUy42KhHkvn6G6gucID0tDbEA9fgKD-xnvgqoXGTeDT~awhcJA73PFVqGGzGFgqFJLN8KBaZ7U220cKVutrmbwZj0TuOKqoQsNYj0M6BkaSwdHzCgYAUGGsGwmts1M5-gCTLZUVGHjHdg2RjkZfnhcnxIqa8QLccF17HHgVpeDphK0wQ__" alt="" className="mainimg" />
+        <p className='mainpara'>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.</p>
+        <button className='button'>Visit Website</button>
+      </div>
+      <div className='d-flex flex-row justify-content-center'>
+        <div>
+          <img src="https://s3-alpha-sig.figma.com/img/7147/9344/2501faa7f289434a9b99bafaa474cc0e?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eH~wuXAEILf-vKik9f6z0TaJy-rKIilEDMS~j6ZZQoMfgaznxD8FQLah5BlsayxPfP8yLiJSwwuhyndVSS6eq~rDRUODS1qB4HhMxgczcV55xB4cJ10a1dVc0eAHjxwsNJBEDM7w3zcNM6Y5Z8oLsCxNQM3~rPbeuwAUhDkvEAyzGAfvoT6mAvi46dh~OPc8fbz2Z7j9m4MuKZ9~Ca-6sZktJOzdf32O4zcwErgx1otoaYtGejZbQxE-MHdB3GyIf~GvKbeIrztYLd2OZH5zYcsee0Z7c0KeGMpYkAqIBVSo~n3hZOgBmxzsmvmAbf7wvOBsLSD72wVDgLPA~6hplw__" alt="" className="subimg" />
+          <p className='cardpara'>Explore large, destructible environments where no two games are ever the same.</p>
+        </div>
+        <div>
+          <img src="https://s3-alpha-sig.figma.com/img/ff28/c642/d7258eeaa99e5ea5585055099ce474ea?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PjWMSRRHXYUv3UkNpEW2Cj1AYamttGy5MOc9jzQvIY5juhqO~I4TtHrhfwsnGx9ZvfYzwp5K34RRjBpBrqPSwGIgyH8Ty5dEpHsuiwCPqEq3tkozfVrmweX3iyftWgNqnFJEZofuIva4H7YeioPgIsmttFWsBIasi-E9OYNSaC49oqOl2~ofU-3Xrapf67Tgg4K93ZN5Z6XMiBjfi2sHbNF0r8NPRnA9fEMruVcSJHZFwKkZuCXq4ER2h7P2aweyVDrYV5vIEB~Ben8dvEuntBn2hMXeqIyYdTIGc7qJxMfLfTnATZM~OYBCk4CIkuS0HxWy~R5USlJlc-SNO9GPFw__" alt="" className="subimg" />
+          <p className='cardpara'>Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale.</p>
+        </div>
+        <div>
+          <img src="https://s3-alpha-sig.figma.com/img/a09a/b5b0/95547eb344eb0c0875a5194c38c5747c?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MAjdRF4TPncO4RRFu38vfCl9Oqv1VcH5S1Tv5-nDzcDqXNc2R34~zuoe7e6u6nM~N5cBnVtvCu3M2QRi3mequyUJnyjbi-DnF~rGFj71MqPsFBgJMF~sDXrRjOwzKUmnJqqt9v~2jEGXXXyLbjMjNhBDe~-zeIy9NlGoRMuP9-gYsw2auC-TX-xfo-~xFJ2KJVKiXKFCrNyi-o~M1w~itFgmrfoI3~1M1nvJTD~1ziz~oaxHof-tRkN5VJuZ5MKFhA2Ew3aU0IPzSYNQ3ykxKlTST-YQtBLsG-KQhZXpTDT8WrIIEA1rjjUzqyaUtiduX1nUFQqyCj6MDsTVp4kW5g__" alt="" className="subimg"/>
+          <p className='cardpara'>Discover even more ways to play across thousands of creator-made game genres</p>
+        </div>
+      </div>
+      <div>
+      <h1 className='contributer-heading'>Our Contribution</h1>
+      <p className='subpara'>Our core offering extends beyond mere profit generation; we emphasize the growth and active involvement of our user community. Collaborating with us represents an investment, rather than a mere expenditure. Our dedication to providing distinctive digital interactions guarantees unparalleled benefits for our clientele.</p>
+      </div>
+      <div className='d-flex flex-row justify-content-center'>
+      <div className=' review'>
+        <h1 className='reviewcard-heading'>5M</h1>
+        <p className='reviewcard-para'>Daily User 
+Engagements</p>
+      </div>
+      <div  className=' review'>
+        <h1 className='reviewcard-heading'>$500K</h1>
+        <p className='reviewcard-para'>Revenue Surge for an
+Platform</p>
+      </div>
+      <div  className=' review'>
+        <h1 className='reviewcard-heading'>5M</h1>
+        <p className='reviewcard-para'>ROAS on all our 
+marketing campaigns</p>
+      </div>
+      </div>
+      <div className='lastcontainer'>
+        <h1>Interested in delving deeper into the project?</h1>
+        <p>If you'd like to explore further details about our initiatives or any of our affiliated brands, don't hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.</p>
+        <div className='d-flex flex-row lastbtn-container'>
+          <button className='subbtn1'>Ring us on Skype</button>
+          <button className='subbtn2'>Contact Us</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
